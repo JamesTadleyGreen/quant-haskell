73f38452b596a356236337af3b45d9746c14ad3d
James Green <jamestadleygreen@outlook.com>
2025-02-03 14:46:24 +0000

    feat: better typing and fix

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,7 @@ import System.Random.MWC (create)
 main :: IO ()
 main = do
   mwc <- create
-  path <- runRVar (MyLib.vasicek 0.03 0.05 0.05 0.02 1 365) mwc
+  path <- runRVar (MyLib.vasicek 0.5 0.05 0.02 0.03 0.005 200) mwc
   toFile def "example.png" $ do
     layout_title .= "Vasicek model"
     plot (line "am" [zip ([1 ..] :: [Float]) path])
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -3,18 +3,26 @@ module MyLib (vasicek) where
 import Control.Monad (replicateM)
 import Data.Random (RVar, normal)
 
-ornsteinUhlenbeckStep :: Float -> Float -> Float -> Float -> Float -> Float -> Float
-ornsteinUhlenbeckStep theta mu sigma dt z x_prev = x_prev + drift + noise
+type Mean = Float
+
+type MeanReversion = Float
+
+type Variance = Float
+
+type TimeStep = Float
+
+ornsteinUhlenbeckStep :: MeanReversion -> Mean -> Variance -> TimeStep -> Float -> Float -> Float
+ornsteinUhlenbeckStep theta mu sigma dt x_prev z = x_prev + drift + noise
   where
     drift = theta * (mu - x_prev) * dt
     noise = sigma * z
 
 -- https://en.wikipedia.org/wiki/Ornstein%E2%80%93Uhlenbeck_process
-ornsteinUhlenbeck :: Float -> Float -> Float -> Float -> Float -> Int -> RVar [Float]
+ornsteinUhlenbeck :: Float -> MeanReversion -> Mean -> Variance -> TimeStep -> Int -> RVar [Float]
 ornsteinUhlenbeck x theta mu sigma dt number_of_steps = do
   zs <- replicateM number_of_steps (normal 0 1)
   pure $ scanl (ornsteinUhlenbeckStep theta mu sigma dt) x zs
 
 -- https://en.wikipedia.org/wiki/Vasicek_model
-vasicek :: Float -> Float -> Float -> Float -> Float -> Int -> RVar [Float]
-vasicek = ornsteinUhlenbeck
+vasicek :: Float -> MeanReversion -> Mean -> Variance -> TimeStep -> Int -> RVar [Float]
+vasicek x theta mu sigma dt = ornsteinUhlenbeck x theta mu (sigma * sqrt dt) dt
